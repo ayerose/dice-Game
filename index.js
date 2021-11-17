ba3bf0caafbdfbd2ee6bca9f80904d2d1c8cc46a
ayerose <reya-k@outlook.de>
2021-11-17 21:09:09 +0000

    “update”

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ const player2Scoreboard = document.getElementById('player2Scoreboard');
 const message = document.getElementById('message');
 const rollBtn = document.getElementById('rollBtn');
 const resetBtn = document.getElementById('resetBtn');
+
+ rollBtn.addEventListener("click", function() {
+     const randomNumber = Math.floor(Math.random() * 6) + 1
+     
+
+  
+     if (player1Turn) {
+         console.log("player 1 rolled " + randomNumber)
+     } else {
+        console.log("player 2 rolled " + randomNumber)
+     }
+     player1Turn = !player1Turn
+ })
+ 
+ 
