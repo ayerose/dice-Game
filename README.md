fd94b482913631698fb3ea32b4ae3697135e344b
ayerose <reya-k@outlook.de>
2021-11-17 20:54:01 +0000

    “updated”

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dice-Game
+
+try to reach a score of 20 as fast as possible!
--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-    
-    if (player1Score >= 20) {
-        message.textContent = "Player 1 Won 🥳"
-        showResetButton()
-    }  else if (player2Score >= 20) {
-        message.textContent = "Player 2 Won 🎉"
-        showResetButton()
-    }
-    player1Turn = !player1Turn
-})
- 
-resetBtn.addEventListener("click", function(){
-    reset()
-})
-
-function reset() {
-    player1Score = 0
-    player2Score = 0
-    player1Turn = true
-    player1Scoreboard.textContent = 0
-    player2Scoreboard.textContent = 0
-    player1Dice.textContent = "-"
-    player2Dice.textContent = "-"
-    message.textContent = "Player 1 Turn"
-    resetBtn.style.display = "none"
-    rollBtn.style.display = "block"
-    player2Dice.classList.remove("active")
-    player1Dice.classList.add("active")
-}
+let player1Score = 0
+let player2score = 0 
+let player1Turn = true
